6f169afadae01dcbfae6dcfa1759ce7620e86d62
Sarhol <31293533+Sarhol@users.noreply.github.com>
2017-11-28 17:00:54 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -7,25 +7,26 @@
         var LatLng5 = {lat: 34.687333, lng: 135.526158};
         var LatLng6 = {lat: 34.685012, lng: 135.843033};
         var kyoto = {lat: 35.0116, lng: 135.7680};  
-        var Ryoanji = 'Ryoanji rock garden'
-        var kinkakuji = 'kinkakuji (Golden pavilion)'
-        var Hiroshima = 'Hiroshima peace park & museum'
-        var Osaka = 'Osaka castle'
-        var Nara = 'Todaiji temple'
-        var gin = 'Ginkakuji (Silver pavilion)'
+        var Ryoanji = 'Ryoanji rock garden';
+        var kinkakuji = 'kinkakuji (Golden pavilion)';
+        var Hiroshima = 'Hiroshima peace park & museum';
+        var Osaka = 'Osaka castle';
+        var Nara = 'Todaiji temple';
+        var gin = 'Ginkakuji (Silver pavilion)';
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 6,
           center: kyoto,
         });
            var info = new google.maps.InfoWindow({
       content: Ryoanji,
-    position:LatLng1,
+    position:LatLng1
 });
         var marker = new google.maps.Marker({
             position: LatLng1,
             map: map,
             title:"Ryoanji",
             icon: 'pin.png',
+            content: Ryoanji,
             
         });  
           var marker2 = new google.maps.Marker({
@@ -64,45 +65,38 @@
             content: Nara,
         });
           
+            marker.addListener('click', function() {
+          map.setZoom(8);
+          map.setCenter(marker.getPosition());
+        });
+          var info1 = new google.maps.InfoWindow({
+      content: Ryoanji
+    });
+          var info2 = new google.maps.InfoWindow({
+      content: Hiroshima
+    });
+          var info3 = new google.maps.InfoWindow({
+      content: kinkakuji
+    });
+          var info4 = new google.maps.InfoWindow({
+      content: gin
+    });
+          var info5 = new google.maps.InfoWindow({
+      content: Osaka
+    });
+          var info6 = new google.maps.InfoWindow({
+      content: Nara
+    });
+          
          google.maps.event.addListener(marker, 'click', function(){
-      info.open(map, marker);
-      info1.open(map, marker1);
-});
+      info1.open(map, marker);
+      info2.open(map, marker2);
+      info3.open(map, marker3);
+      info4.open(map, marker4);
+      info5.open(map, marker5);
+      info6.open(map, marker6);
+    
 });
       }
-      
-      
-      
-        var image1 = new Image()
-        image1.src = "Osaka.jpg"
-
-        var image2 = new Image()
-        image2.src = "Gin.jpg"
-
-        var image3 = new Image()
-        image3.src = "Kin.jpg"
-        
-        var image4 = new Image()
-        image3.src = "Nara.jpg"
-
-        var image5 = new Image()
-        image3.src = "Deer.jpg"
-        
-        var image6 = new Image()
-        image3.src = "Ryoanji.jpg"
-
-        var image7 = new Image()
-        image3.src = "Hiroshima.jpg"
-    
-  var step = 1
-        function slideit() {
-            document.images["slide"].src = eval("image" + step + ".src")
-            if (step < 7)
-                step++
-            else
-                step = 1
-            setTimeout("slideit()", 5000)
-        }
-        slideit()
  
    
